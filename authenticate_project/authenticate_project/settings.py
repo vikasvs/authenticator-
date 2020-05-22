c3a46330c273eeafd60b1ed531ddb9721564e8b6
Vikas Sharma <vvsharma21@berkeley.edu>
2020-05-22 13:34:08 -0400

    added crispy form

--- authenticate_project/authenticate_project/settings.py
+++ authenticate_project/authenticate_project/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'authenticate.apps.AuthenticateConfig',
     'user.apps.UserConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'authenticate-home'
 LOGIN_URL = 'login'
