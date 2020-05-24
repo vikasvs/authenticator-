6c77e7eab75e833a76182bdcbcccd8599b10c6a5
Vikas Sharma <vvsharma21@berkeley.edu>
2020-05-23 23:06:00 -0400

    stf

--- authenticate_project/authenticate_project/settings.py
+++ authenticate_project/authenticate_project/settings.py
@@ -126,3 +126,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'user-profile'
 LOGIN_URL = 'login'
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
+MEDIA_URL = '/media/'
--- authenticate_project/authenticate_project/urls.py
+++ authenticate_project/authenticate_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
-
+from authenticate_project import settings as sett
 from user import views as user_views
 
 urlpatterns = [
