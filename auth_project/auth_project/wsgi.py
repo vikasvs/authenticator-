afe140fc06e4bfa4ec648cb250fa94665d4d4e20
Vikas Sharma <vvsharma21@berkeley.edu>
2020-06-21 20:37:31 -0400

    stuff

--- auth_project/auth_project/wsgi.py
+++ auth_project/auth_project/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 import sys
-sys.path.insert(0,os.path.join(os.path.abspath(__file__), "auth_project/auth_project"))
-sys.path.insert(0,os.path.join(os.path.abspath(__file__), "auth_project"))
+sys.path.insert(0,os.path.join(os.getcwd(), "auth_project/auth_project"))
+sys.path.insert(0,os.path.join(os.getcwd(), "auth_project"))
 
 
 
