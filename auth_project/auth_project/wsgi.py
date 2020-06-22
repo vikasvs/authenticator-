62eae9c077718f72bec0d2070c808eba3a70b6f8
Vikas Sharma <vvsharma21@berkeley.edu>
2020-06-21 20:43:52 -0400

    set

--- auth_project/auth_project/wsgi.py
+++ auth_project/auth_project/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 import sys
-sys.path.insert(0,os.path.join(os.getcwd(), "auth_project/auth_project"))
-sys.path.insert(0,os.path.join(os.getcwd(), "auth_project"))
-
+sys.path.insert(0,os.path.join(os.getcwd(), "authenticator/auth_project/auth_project"))
+sys.path.insert(0,os.path.join(os.getcwd(), "authenticator/auth_project"))
+print("CWD ******: {}".format(os.getcwd()))
 
 
 from django.core.wsgi import get_wsgi_application
