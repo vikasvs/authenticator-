cf5d327f8244144ec7a49d326b5d0897020094eb
Vikas Sharma <vvsharma21@berkeley.edu>
2020-07-03 18:24:22 -0400

    set

--- auth_project/auth_project/wsgi.py
+++ auth_project/auth_project/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 import sys
+
 sys.path.insert(0,os.path.join(os.getcwd(), "auth_project/auth_project"))
 sys.path.insert(0,os.path.join(os.getcwd(), "auth_project"))
 print("CWD ******: {}".format(os.getcwd()))
