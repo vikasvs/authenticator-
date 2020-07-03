0fcc55b7a6ecab2904a76d8f3f6dfd5889a706ac
Vikas Sharma <vvsharma21@berkeley.edu>
2020-07-03 18:12:10 -0400

    settings

--- auth_project/auth_project/settings.py
+++ auth_project/auth_project/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY =  os.environ.get('SECRET_KEY')
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 #DEBUG = 'TRUE'
